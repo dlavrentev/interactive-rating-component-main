c45862a3467f9e520429616d6f355a0d03fee13e
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-15 17:27:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ const submitBtn = document.getElementById('submit-star-btn');
 const card_content_1 = document.querySelector('.div1');
 const card_content_2 = document.querySelector('.div2');
 const score = document.querySelector('.score');
-stars_score = 3;
+let stars_score = 0;
 
 
 
 function onSubmit() {
-    card_content_1.classList.add('hide');
-    score.textContent = 4;
-    card_content_2.classList.remove('hide');
-}
+    if (stars_score === 0) {
+        alert("Please choose a rating from 1-5!");
+    } else {
+        card_content_1.classList.add('hide');
+        score.textContent = stars_score;
+        card_content_2.classList.remove('hide');
+    }
+    }
+   
 
 submitBtn.addEventListener('click', onSubmit);
 
@@ -23,8 +28,10 @@ const btn5 = document.getElementById("btn5");
 function btnOne() {
     if (btn1.classList.contains("circle")) {
         btn1.setAttribute("class", "orange");
+        stars_score = 1;
     } else if (btn1.classList.contains("orange")) {
         btn1.setAttribute("class", "circle")
+        stars_score = 0;
     }
     btn2.setAttribute("class", "circle");
     btn3.setAttribute("class", "circle");
@@ -34,8 +41,10 @@ function btnOne() {
 function btnTwo() {
     if (btn2.classList.contains("circle")) {
         btn2.setAttribute("class", "orange");
+        stars_score = 2;
     } else if (btn2.classList.contains("orange")) {
         btn2.setAttribute("class", "circle")
+        stars_score = 0;
     }
     btn1.setAttribute("class", "circle");
     btn3.setAttribute("class", "circle");
@@ -45,8 +54,10 @@ function btnTwo() {
 function btnThree() {
     if (btn3.classList.contains("circle")) {
         btn3.setAttribute("class", "orange");
+        stars_score = 3;
     } else if (btn3.classList.contains("orange")) {
         btn3.setAttribute("class", "circle")
+        stars_score = 0;
     }
     btn1.setAttribute("class", "circle");
     btn2.setAttribute("class", "circle");
@@ -56,8 +67,10 @@ function btnThree() {
 function btnFour() {
     if (btn4.classList.contains("circle")) {
         btn4.setAttribute("class", "orange");
+        stars_score = 4;
     } else if (btn4.classList.contains("orange")) {
         btn4.setAttribute("class", "circle")
+        stars_score = 0;
     }
     btn1.setAttribute("class", "circle");
     btn2.setAttribute("class", "circle");
@@ -67,8 +80,10 @@ function btnFour() {
 function btnFive() {
     if (btn5.classList.contains("circle")) {
         btn5.setAttribute("class", "orange");
+        stars_score = 5;
     } else if (btn5.classList.contains("orange")) {
         btn5.setAttribute("class", "circle")
+        stars_score = 0;
     }
     btn1.setAttribute("class", "circle");
     btn2.setAttribute("class", "circle");
